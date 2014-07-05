98180cc2db21357972bd98d9fc994aec6e821942
MJustdo <MJustdo@gmail.com>
2014-07-05 16:27:41 +0800

    optimize dao

--- SQL/query.sql
+++ SQL/query.sql
@@ -2,6 +2,8 @@ select * from user;
 select * from role;
 select * from permission;
 select * from blog;
+select * from comment;
  select r.role_name from user u, user_role_relation ur, role r
  where u.id = ur.user_id and r.id = ur.role_id and u.user_name ='keepthinker';
+ 
  
